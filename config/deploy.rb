7c91bc30caa6ca9c5d33df37e96bd5f59512e7dd
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2020-07-20 08:11:46 +0800

    Hotfix: Update Deploy File

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:iprog21/jaysonpresto.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_files, "config/secrets.yml"
+append :linked_files, "config/secrets.yml", "config/database.yml"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Only keep the last 5 releases to save disk space
