6831788de25f4c933fc07a3cdcc897a419139a32
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2020-08-03 15:41:43 +0800

    Hotfix: Hide Other Pages

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jezelpresto@gmail.com'
   layout 'mailer'
+  def submit_contact_message
+    @email = params[:email]
+    mail(to: @email, subject: "Test Message")
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # on devise config
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "jezelpresto@gmail.com",
+    :password             => "gmailProgrammer0518",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
