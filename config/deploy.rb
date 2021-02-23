2f7dd070c5f2c335a61fc4b4de177f23700f90c1
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2021-02-23 22:38:21 +0800

    Update capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.15.0"
 
 set :application, "jaysonpresto"
 set :repo_url, "git@github.com:iprog21/jaysonpresto.git"
